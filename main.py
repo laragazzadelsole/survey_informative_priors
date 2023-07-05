08c499c3c297d2db891b2a180797c2d2ba5071bd
Sara Gironi <sara.gironi97@gmail.com>
2023-07-05 16:57:59 +0200

    update

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import streamlit_survey as ss
 import streamlit as st
-import numpy as np
-import pickle as pkle
-import os.path
+
 survey = ss.StreamlitSurvey()
 
 #[theme]
@@ -19,6 +17,10 @@ st.write("""The scope of this survey is to record informative priors of differen
 them in Bayesian impact evaluation to learn more from expensive new programs tested on relatively small samples.""")
 st.write("The research is held on a Colombian Government Program which aimed to increase export in small, medium and large firms.")
 
+#fix the session state
+if 'key' not in st.session_state:
+    st.session_state['key'] = 'value'
+
 #insert consent
 placeholder = st.empty()
 with placeholder.container():
@@ -29,46 +31,40 @@ with placeholder.container():
         agree = st.checkbox("I understand and consent.")
         if agree:
             st.markdown("You have consented. Select \"Next\" to start the survey.")
-            if st.button('Next'):
-                            
-                with st.form(key = "my_form"):
-                    st.write("Specify your professional category:")
-                    option = st.selectbox(
-                        'Select one of the options below:',
-                        ('Policymaker', 'Expert', 'Firm'))
-                    #st.write('The survey for the', option, 'will be displayed.' )
+if st.button('Next'):
+    st.write("Specify your professional category:")
+    option = st.selectbox(
+        'Select one of the options below:',
+        ('Policymaker', 'Expert', 'Firm'))
 
-                    st.write("Express your beliefs regarding the impact of the consulting programm on export for the firms offered the treatment compared to firms offered just the diagnostic phase and trade:")
-                    export_impact = st.radio("Select one of the options below:", options=["Positive", "Negative", "No changes"], horizontal=False)
-
-                    if export_impact == "Positive":
-                        st.write("""Choose what percentage on average do you think the export is going to increase in the treated firms:""")
-                        positive_slider = st.slider("Select the percentage sliding on the bar below:", 0, 100, format = '%d')
-                        
-                        st.write("""Choose with what probability on average do you think the export is going to increase in the treated firms:""")
-                        prob_slider = st.slider("Select the probability sliding on the bar below:", 0, 100)
-                        st.write("Please shortly summarize the reasons for your previous answer:")
-                        positive_text = st.text_input("Write your answer below:", key = 'positive_text' )
-                       
-                        st.radio("Select one of the following options", options = ["Diversify the range of products exported", "Diversify the destinations of exportation", "All of the above"])
-                        
-                    elif export_impact == "Negative":
-                        st.write("""Choose what percentage on average do you think the export is going to decrease in the treated firms:""")
-                        negative_slider = st.slider("Select the percentage sliding on the bar below:", 0, -100, format = '%d')
-                        
-                        st.write("""Choose with what probability on average do you think the export is going to decrease in the treated firms:""")
-                        prob_slider_neg = st.slider("Select the probability sliding on the bar below:", 0, 100)
-                        st.write("Please shortly summarize the reasons for your previous answer:")
-                        
-                        negative_text = st.text_input("Write your answer below:", key = 'negative_text')
-                    else: 
-                        st.write("""Choose with what probability on average do you think the export is not going to change in the treated firms:""")
-                        prob_slider_neutral = st.slider("Select the probability sliding on the bar below:", 0, 100)
-                        st.write("Please shortly summarize the reasons for your previous answer:")
-                    
-                        text = st.text_input("Write your answer below:", key = 'text')
+    st.write("Express your beliefs regarding the impact of the consulting programm on export for the firms offered the treatment compared to firms offered just the diagnostic phase and trade:")
+    export_impact = st.radio("Select one of the options below:", options=["Positive", "Negative", "No changes"], horizontal=False, key = 'export_impact')
 
 
-                    submitted = st.form_submit_button("Submit")
-                    if submitted:
-                        st.success("You completed the form successfully!")
+    if export_impact == "Positive":
+        st.write("""Choose what percentage on average do you think the export is going to increase in the treated firms:""")
+        positive_slider = st.slider("Select the percentage sliding on the bar below:", 0, 100, format = '%d', key = 'positive_slider')
+        st.write("""Choose with what probability on average do you think the export is going to increase in the treated firms:""")
+        prob_slider = st.slider("Select the probability sliding on the bar below:", 0, 100, key = 'prob_slider')
+        st.write("Please shortly summarize the reasons for your previous answer:")
+        positive_text = st.text_input("Write your answer below:", key = 'positive_text' )
+        st.radio("Select one of the following options", options = ["Diversify the range of products exported", "Diversify the destinations of exportation", "All of the above"])
+    if export_impact == "Negative":
+        st.write("""Choose what percentage on average do you think the export is going to decrease in the treated firms:""")
+        negative_slider = st.slider("Select the percentage sliding on the bar below:", 0, -100, format = '%d', key = 'negative_slider')
+        st.write("""Choose with what probability on average do you think the export is going to decrease in the treated firms:""")
+        prob_slider_neg = st.slider("Select the probability sliding on the bar below:", 0, 100, key = 'prob_slider_neg')
+        st.write("Please shortly summarize the reasons for your previous answer:")
+        negative_text = st.text_input("Write your answer below:", key = 'negative_text')
+    else: 
+        st.write("""Choose with what probability on average do you think the export is not going to change in the treated firms:""")
+        prob_slider_neutral = st.slider("Select the probability sliding on the bar below:", 0, 100, key = 'prob_slider_neutral')
+        st.write("Please shortly summarize the reasons for your previous answer:")
+        text = st.text_input("Write your answer below:", key = 'text')
+
+
+
+    st.success("You completed the form successfully!")
+
+
+
