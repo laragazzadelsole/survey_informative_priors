a7b0eeb4ada4fe6160ad46209b992aefe6f8e550
Sara Gironi <sara.gironi97@gmail.com>
2023-07-27 19:41:05 +0200

    test3

--- utils.py
+++ utils.py
@@ -16,7 +16,7 @@ conn = connect(credentials=credentials)
 
 # Perform SQL query on the Google Sheet.
 # Uses st.cache_data to only rerun when the query changes or after 10 min.
-@st.cache_data(ttl=600)
+#@st.cache_data(ttl=600)
 
 def show_titles_and_subtitles():
     st.title(TITLE)
